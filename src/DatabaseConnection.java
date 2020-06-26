dddfc8e57a1c1d855f08dc5f2e0391bf4d6bb79e
= <=>
2020-06-27 06:24:21 +0700

    first commit

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,31 @@
+
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+	protected Connection conn;
+	///Tao Lien ket voi SQL Server
+	public DatabaseConnection() {
+		try {
+			String dbURL = "jdbc:sqlserver://localhost\\THANG;user=sa;password=123;database=QLQPizza";
+			
+			conn = DriverManager.getConnection(dbURL);
+			if (conn != null) {
+			    System.out.println("Connected");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	// Added your method 
+	//
+	//
+	
+	
+	
+	public static void main(String[] args) {
+		new DatabaseConnection();
+	}
+}
